67aeed2665ba5b009ba0e6954f6693ab776a3dfd
Kat Zongker <katrina@zongker.net>
2024-08-20 21:29:40 -0500

    mildStyle

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -2,18 +2,17 @@ import { Container, Nav, Navbar } from "react-bootstrap";
 
 export const Header = () => {
     return <>
-    <div className="header">Dragon Practice Website</div>
-    <Navbar bg="dark" data-bs-theme="dark">
-        <Container className="justify-content-evenly">
-            <Nav>
-                <Nav.Link href="index.html">Homepage</Nav.Link>
-                <Nav.Link href="Humans.html">Human Characters</Nav.Link>
-                <Nav.Link href="DragonChars.html">Dragon Characters</Nav.Link>
-                <Nav.Link href="DragonSurvey.html">Survey</Nav.Link>
-                <Nav.Link href="FanGallery.html">Fan Art</Nav.Link>
-                <Nav.Link href="GamePage.html">Game</Nav.Link>
-            </Nav>
-        </Container>
-    </Navbar>
+        <div className="header">Dragon Practice Website</div>
+        <Navbar sticky="top" bg="dark" data-bs-theme="dark">
+            <Container className="justify-content-evenly">
+                <Nav>
+                    <Nav.Link href="index.html" className="navlinks">Homepage</Nav.Link>
+                    <Nav.Link href="Humans.html" className="navlinks">Human Characters</Nav.Link>
+                    <Nav.Link href="DragonChars.html" className="navlinks">Dragon Characters</Nav.Link>
+                    <Nav.Link href="DragonSurvey.html" className="navlinks">Survey</Nav.Link>
+                    <Nav.Link href="FanGallery.html" className="navlinks">Fan Art</Nav.Link>
+                </Nav>
+            </Container>
+        </Navbar>
     </>
 };

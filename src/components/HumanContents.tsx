c804b61757369b7fe1c4d365f19b649f3f49bcb0
Kat Zongker <katrina@zongker.net>
2024-08-31 13:58:50 -0500

    alertFix

--- src/components/HumanContents.tsx
+++ src/components/HumanContents.tsx
@@ -1,5 +1,5 @@
 import { useState } from "react";
-import { Col, Container, Form, Row } from "react-bootstrap";
+import { Alert, Col, Container, Form, Row } from "react-bootstrap";
 
 
 interface Props {
@@ -12,6 +12,8 @@ export const HumanContents = (props: Props) => {
     const [fightError, setFightError] = useState(false);
     const [battlerOne, setBattlerOne] = useState(0);
     const [battlerTwo, setBattlerTwo] = useState(0);
+    const [humanEdits, setHumanEdits] = useState(false);
+    const [noDelete, setNoDelete] = useState(false);
 
     const doBattle = () => {
         battlerOne === battlerTwo ? setFightError(true) : setFightError(false);
@@ -29,14 +31,13 @@ export const HumanContents = (props: Props) => {
 
             <Col lg={8} id="fightAlert"  /* style="display: none" */>
                 {fightError &&
-                    <div className="alert alert-warning alert-dimissible fade show text-center" role="alert">
+                    <Alert variant="warning" className="text-center" dismissible onClose={() => { setFightError(false) }}>
                         <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-exclamation-diamond" viewBox="0 0 16 16">
                             <path d="M6.95.435c.58-.58 1.52-.58 2.1 0l6.515 6.516c.58.58.58 1.519 0 2.098L9.05 15.565c-.58.58-1.519.58-2.098 0L.435 9.05a1.48 1.48 0 0 1 0-2.098zm1.4.7a.495.495 0 0 0-.7 0L1.134 7.65a.495.495 0 0 0 0 .7l6.516 6.516a.495.495 0 0 0 .7 0l6.516-6.516a.495.495 0 0 0 0-.7L8.35 1.134z" />
                             <path d="M7.002 11a1 1 0 1 1 2 0 1 1 0 0 1-2 0M7.1 4.995a.905.905 0 1 1 1.8 0l-.35 3.507a.552.552 0 0 1-1.1 0z" />
                         </svg>
                         Viking Cannot Fight Themself!
-                        <button type="button" className="btn-close" /* onclick="hideBoxTwo();" */ aria-label="Close"></button>
-                    </div>
+                    </Alert>
                 }
             </Col>
 
@@ -67,13 +68,15 @@ export const HumanContents = (props: Props) => {
                 </form>
 
                 <Container id="alertBox">
-                    <div className="alert alert-primary alert-dismissible fade show text-center" role="alert">
-                        Please ignore this alert box!
-                        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-arrow-right-circle" viewBox="0 0 16 16">
-                            <path fill-rule="evenodd" d="M1 8a7 7 0 1 0 14 0A7 7 0 0 0 1 8m15 0A8 8 0 1 1 0 8a8 8 0 0 1 16 0M4.5 7.5a.5.5 0 0 0 0 1h5.793l-2.147 2.146a.5.5 0 0 0 .708.708l3-3a.5.5 0 0 0 0-.708l-3-3a.5.5 0 1 0-.708.708L10.293 7.5z" />
-                        </svg>
-                        <button type="button" className="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
-                    </div>
+                    {humanEdits &&
+                        <Alert variant="primary" className="text-center">
+                            Please ignore this alert box!
+                            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-arrow-right-circle" viewBox="0 0 16 16">
+                                <path fill-rule="evenodd" d="M1 8a7 7 0 1 0 14 0A7 7 0 0 0 1 8m15 0A8 8 0 1 1 0 8a8 8 0 0 1 16 0M4.5 7.5a.5.5 0 0 0 0 1h5.793l-2.147 2.146a.5.5 0 0 0 .708.708l3-3a.5.5 0 0 0 0-.708l-3-3a.5.5 0 1 0-.708.708L10.293 7.5z" />
+                            </svg>
+                            <button type="button" className="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
+                        </Alert>
+                    }
                 </Container>
 
                 <form id="addVikings" className="text-center formfont">
@@ -183,15 +186,17 @@ export const HumanContents = (props: Props) => {
                     <button type="button" id="addHumanBtn" className="mb-4" /* onclick="requireHuman();" */>Submit</button>
                 </form>
 
-                <Col lg={12} id="deleteAlert" /* style="display: none" */>
-                    <div className="alert alert-warning alert-dimissible fade show text-center" role="alert">
-                        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-exclamation-diamond" viewBox="0 0 16 16">
-                            <path d="M6.95.435c.58-.58 1.52-.58 2.1 0l6.515 6.516c.58.58.58 1.519 0 2.098L9.05 15.565c-.58.58-1.519.58-2.098 0L.435 9.05a1.48 1.48 0 0 1 0-2.098zm1.4.7a.495.495 0 0 0-.7 0L1.134 7.65a.495.495 0 0 0 0 .7l6.516 6.516a.495.495 0 0 0 .7 0l6.516-6.516a.495.495 0 0 0 0-.7L8.35 1.134z" />
-                            <path d="M7.002 11a1 1 0 1 1 2 0 1 1 0 0 1-2 0M7.1 4.995a.905.905 0 1 1 1.8 0l-.35 3.507a.552.552 0 0 1-1.1 0z" />
-                        </svg>
-                        Nobody to delete!
-                        <button type="button" className="btn-close" /* onclick="hideBoxThree();" */ aria-label="Close"></button>
-                    </div>
+                <Col lg={12} id="deleteAlert">
+                    {noDelete &&
+                        <Alert variant="warning" className="text-center">
+                            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-exclamation-diamond" viewBox="0 0 16 16">
+                                <path d="M6.95.435c.58-.58 1.52-.58 2.1 0l6.515 6.516c.58.58.58 1.519 0 2.098L9.05 15.565c-.58.58-1.519.58-2.098 0L.435 9.05a1.48 1.48 0 0 1 0-2.098zm1.4.7a.495.495 0 0 0-.7 0L1.134 7.65a.495.495 0 0 0 0 .7l6.516 6.516a.495.495 0 0 0 .7 0l6.516-6.516a.495.495 0 0 0 0-.7L8.35 1.134z" />
+                                <path d="M7.002 11a1 1 0 1 1 2 0 1 1 0 0 1-2 0M7.1 4.995a.905.905 0 1 1 1.8 0l-.35 3.507a.552.552 0 0 1-1.1 0z" />
+                            </svg>
+                            Nobody to delete!
+                            <button type="button" className="btn-close" /* onclick="hideBoxThree();" */ aria-label="Close"></button>
+                        </Alert>
+                    }
                 </Col>
 
                 <form className="text-center formfont">

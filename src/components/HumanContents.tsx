afe896c96a4d8978f7f7c86ff212e00daf15e063
Kat Zongker <katrina@zongker.net>
2024-09-05 16:28:56 -0500

    deleteSomewhat

--- src/components/HumanContents.tsx
+++ src/components/HumanContents.tsx
@@ -123,8 +123,12 @@ export const HumanContents = (props: Props) => {
         if (json !== null) setCustomHumans(JSON.parse(json));
     }, []);
 
-    const murder = () => {
-        customHumans.length < 1 ? setNoDelete(true) : setNoDelete(false);
+    const murder = (index: any) => {
+        const deletelist = [...customHumans];
+        customHumans.length < 1 ? setNoDelete(true) : deletelist.splice(index, 1);
+        setCustomHumans(deletelist);
+        localStorage.removeItem("customHumans");
+        localStorage.setItem("customHumans", JSON.stringify(deletelist));
     };
 
     return <>
@@ -294,13 +298,12 @@ export const HumanContents = (props: Props) => {
 
                 <Col lg={12} id="deleteAlert">
                     {noDelete &&
-                        <Alert variant="warning" className="text-center">
+                        <Alert variant="warning" className="text-center" dismissible onClose={() => { setNoDelete(false) }}>
                             <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-exclamation-diamond" viewBox="0 0 16 16">
                                 <path d="M6.95.435c.58-.58 1.52-.58 2.1 0l6.515 6.516c.58.58.58 1.519 0 2.098L9.05 15.565c-.58.58-1.519.58-2.098 0L.435 9.05a1.48 1.48 0 0 1 0-2.098zm1.4.7a.495.495 0 0 0-.7 0L1.134 7.65a.495.495 0 0 0 0 .7l6.516 6.516a.495.495 0 0 0 .7 0l6.516-6.516a.495.495 0 0 0 0-.7L8.35 1.134z" />
                                 <path d="M7.002 11a1 1 0 1 1 2 0 1 1 0 0 1-2 0M7.1 4.995a.905.905 0 1 1 1.8 0l-.35 3.507a.552.552 0 0 1-1.1 0z" />
                             </svg>
                             Nobody to delete!
-                            <button type="button" className="btn-close" /* onclick="hideBoxThree();" */ aria-label="Close"></button>
                         </Alert>
                     }
                 </Col>

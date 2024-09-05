c2772d03e41454670c47b5e47be7cc562ec4b5cd
Kat Zongker <katrina@zongker.net>
2024-09-05 18:37:52 -0500

    cleanCode

--- src/Humans.tsx
+++ src/Humans.tsx
@@ -1,15 +1,21 @@
 import { useEffect, useState } from "react";
 import { HumanCard } from "./components/HumanCard";
-import { HumanContents } from "./components/HumanContents";
 import { SiteWrapper } from "./components/SiteWrapper";
 import { Container, Row } from "react-bootstrap";
+import { HumanBattle } from "./components/HumanBattle";
+import { AddHumans } from "./components/AddHumans";
+import { MurderHumans } from "./components/MurderHumans";
 
 export const Humans = () => {
-    const [humans, setHumans] = useState([]);
+    const [customHumans, setCustomHumans] = useState<any[]>([]);
+    const [humans, setHumans] = useState<any[]>([]);
+    const combinedHumans = [...humans].concat(customHumans);
     const loadData = async () => {
         const resp = await fetch("/humans.json");
         const data = await resp.json();
         console.log(setHumans(data));
+        const json = localStorage.getItem("customHumans");
+        if (json !== null) setCustomHumans(JSON.parse(json));
     }
 
     useEffect(() => {
@@ -27,8 +33,11 @@ export const Humans = () => {
     return <>
         <SiteWrapper>
             <Row className="stripeTwo" />
-            <Container className="text-center">{populateCards()}</Container>
-            <HumanContents humans={humans} />
+            <Container className="text-center">{populateCards()}
+                <HumanBattle humans={combinedHumans} />
+                <AddHumans customHumans={customHumans} setCustomHumans={setCustomHumans} />
+                <MurderHumans customHumans={customHumans} setCustomHumans={setCustomHumans} />
+            </Container>
         </SiteWrapper>
     </>
 };
